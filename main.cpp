f676f48f33b8f56760b47f51fea2a2465bedd864
Mikhail Brel <minya@skbkontur.ru>
2021-06-29 15:42:05 +0500

    Пробую в сентри

--- main.cpp
+++ main.cpp
@@ -10,14 +10,17 @@
 #include <optional>
 #include <syslog.h>
 
+#include "sentry.h"
+
 using namespace std;
 
 struct ProgramOptions {
-    std::string token;
-    std::string dest_path;
-    std::vector<int> allowed_users;
+    string token;
+    string dest_path;
+    vector<int> allowed_users;
     bool daemon;
-    std::string transmission_rpc_uri;
+    string transmission_rpc_uri;
+    string sentry_dsn;
 };
 
 optional<ProgramOptions> read_options(int argc, char** argv);
@@ -26,6 +29,7 @@ void run_as_daemon(dlbot::DLBot& bot);
 void run_as_user(dlbot::DLBot& bot);
 
 int main(int argc, char** argv) {
+
     auto settings_maybe = read_options(argc, argv);
     if (!settings_maybe.has_value()) {
         return EXIT_FAILURE;
@@ -36,24 +40,37 @@ int main(int argc, char** argv) {
         { .token = po.token, .dest_path = po.dest_path, .allowed_users = po.allowed_users },
         { po.transmission_rpc_uri });
 
+    sentry_options_t *options = sentry_options_new();
+    sentry_options_set_dsn(options, po.sentry_dsn.c_str());
+    sentry_init(options);
+
     if (po.daemon) {
         run_as_daemon(bot);
     } else {
         run_as_user(bot);
     }
 
+
+    sentry_close();
     return EXIT_SUCCESS;
 }
 
 void run_as_daemon(dlbot::DLBot& bot) {
     skeleton_daemon(strdup("dlbot"));
-    syslog(LOG_NOTICE, "dlbot started.");
+    syslog(LOG_NOTICE, "dlbot daemon started.");
     try {
         bot.Run();
     } catch(const exception& e) {
         syslog(LOG_ERR, "%s", e.what());
+        sentry_capture_event(
+            sentry_value_new_message_event(SENTRY_LEVEL_ERROR, "custom", e.what()));
+    } catch (...) {
+        string msg = " ...something, not an exception, dunno what.";
+        syslog(LOG_ERR, "%s", msg.c_str());
+        sentry_capture_event(
+            sentry_value_new_message_event(SENTRY_LEVEL_ERROR, "custom", msg.c_str()));
     }
-    syslog(LOG_NOTICE, "dlbot terminated.");
+    syslog(LOG_NOTICE, "dlbot daemon terminated.");
     closelog();
 }
 
@@ -73,6 +90,7 @@ optional<ProgramOptions> read_options(int argc, char** argv) {
         ("allowed-users,u", po::value<vector<int>>()->multitoken(), "Allowed users")
         ("transmission-rpc-uri,R", po::value<string>()->required(), "Transmission RPC base uri")
         ("daemon,D", "Run as daemon")
+        ("sentry-dsn", po::value<string>()->required(), "Sentry DSN")
     ;
 
     po::variables_map vm;
@@ -93,12 +111,14 @@ optional<ProgramOptions> read_options(int argc, char** argv) {
     vector<int> allowed_users = vm["allowed-users"].as<vector<int>>();
     bool daemon = vm.count("daemon");
     string transmission_rpc_uri = vm["transmission-rpc-uri"].as<string>();
+    string sentry_dsn = vm["sentry-dsn"].as<string>();
 
     return ProgramOptions{
         .token = token,
         .dest_path = dest_path,
         .allowed_users = allowed_users,
         .daemon = daemon,
-        .transmission_rpc_uri = transmission_rpc_uri
+        .transmission_rpc_uri = transmission_rpc_uri,
+        .sentry_dsn = sentry_dsn,
     };
 }
